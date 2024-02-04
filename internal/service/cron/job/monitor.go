605c6a4ad0de71e09795b350289c3fa9f0004199
zeusho <zeus@smallho.cn>
2024-02-05 06:06:34 +0800

    代码分层

--- internal/service/cron/job/monitor.go
+++ internal/service/cron/job/monitor.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/duke-git/lancet/v2/convertor"
 	"palworld-chan/internal/service/dao"
-	"palworld-chan/internal/service/dashboard"
 	"palworld-chan/pkg/logger"
 	"palworld-chan/pkg/utility/rcon"
+	"palworld-chan/pkg/utility/utils/monitor"
 	"sync"
 	"time"
 )
@@ -39,7 +39,7 @@ func MonitorServer() (err error) { //监控服务端
 	interval := now.Sub(LastMonitorTime())
 	if int64(interval.Seconds()) >= CheckPeriod {
 		// 获取进程信息
-		cpuUsage, memoryUsage, upTime, memPercent := dashboard.GetProcessInfo(serverSetting.ProcessName)
+		cpuUsage, memoryUsage, upTime, memPercent := monitor.GetProcessInfo(serverSetting.ProcessName)
 		logger.Debug("每%d秒检测服务端进程占用,CPU占用(%s),内存占用(%s),内存占用率(%.2f),运行时间(%s)", CheckPeriod, cpuUsage, memoryUsage, memPercent, upTime)
 		lastMonitorTimeInstance = time.Now() //重置时间
 
